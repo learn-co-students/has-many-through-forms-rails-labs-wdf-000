bf9bc406615b254abefc1b21ed60359c0110e426
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-02 10:51:19 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc {|attr| attr.empty?}
 
+  def user_attributes=(user_attributes)
+    unless user_attributes[:username].empty?
+      user = User.find_or_create_by(user_attributes)
+      user.comments << self
+      user.save 
+    end
+  end
 end
