da1ca1e15077f7f47b81c02581e0d97cea1db575
satub <satu.barnhill@gmail.com>
2016-09-06 14:22:32 -0400

    Done with cats in the db

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :empty_username?
 
+  def empty_username?(user_attr)
+    user_attr[:username].empty?
+  end
 end
