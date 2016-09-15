6b796c71b20b53595edc6b9e238abdac1667b274
fidelscodes <fidelscodes@gmail.com>
2016-09-15 18:14:41 -0400

    create form partial for post update post model update post show view

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,19 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  has_many :commenters, through: :comments, source: :user
+  accepts_nested_attributes_for :categories
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
+  def unique_commenters
+    commenters.uniq
+  end
 
 
 end
