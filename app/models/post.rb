45ecb7bdc700d553ad82742c7e4b9e31fc8c1e3b
Flatiron School <github email address>
2016-09-21 13:07:46 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,37 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      #category_attributes.each do |index, category_attribute|
+      # if category_attributes[:name].present?
+        category = Category.find_or_create_by(category_attribute)
+        # if !self.categories.include?(category)
+          self.post_categories.build(:category => category)
+        # ^^^ instantiating an instance of the join model and putting the category in
+        # self.categories << category -- inefficient
+        # end
+      # end
+    end
+  end
+
+
+  def unique_commenters
+    array = []
+    array << self.users.unique
+    array
+    # binding.pry
+  end
 
 end
+
+
+#
+# Users who have commented on this post:
+# <ul>
+# <% @post.unique_commenters.each do |commenter| %>
+#   <li><%= link_to commenter.username, user_path(commenter) %></li>
+# <% end %>
+# </ul>
