7673640b4d1f1fbfe23a6ae1bddf504a005d68c1
Kelene Chung <simoy93@gmail.com>
2016-10-14 10:25:52 -0400

    DONE

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+  	@user = User.create(user_params)
+  end 
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:username, :email)
+  end
+
 end
